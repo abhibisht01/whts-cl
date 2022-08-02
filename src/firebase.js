7d30cc9694876d5d9adbbe9acb530684d777fe5a
Abhishek Bisht <99400134+abhibisht01@users.noreply.github.com>
2022-08-02 16:36:22 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,7 @@ import 'firebase/compat/auth';
 // import 'firebase/compat/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAM4nVc5DoL7bIMMrWR0F6fQkbhBty2zus",
-    authDomain: "whts-cl.firebaseapp.com",
-    projectId: "whts-cl",
-    storageBucket: "whts-cl.appspot.com",
-    messagingSenderId: "278886590182",
-    appId: "1:278886590182:web:8f496dc520b1e8ddf1fd27",
-    measurementId: "G-C4N0G85QRN"
+ Add Yours
 };
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig);
